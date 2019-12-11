8bc0534b008a08d93a506af4400b52846dff9c80
rakaaaa <rakesh.coe@gmail.com>
2019-12-11 14:28:03 +0530

    10

--- flask_heroku_example/app.py
+++ flask_heroku_example/app.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 from flask import Flask, render_template,jsonify, request, g
-
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
