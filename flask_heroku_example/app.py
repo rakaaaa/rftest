e2c7a179c212b5aacb168da61451011747cd0d88
rakaaaa <rakesh.coe@gmail.com>
2019-12-12 12:42:23 +0530

    :17

--- flask_heroku_example/app.py
+++ flask_heroku_example/app.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 from flask import Flask, render_template,jsonify, request, g
-import pandas as pd
+#import pandas as pd
 import numpy as np
 from simple_salesforce import Salesforce, SalesforceLogin
 import json
